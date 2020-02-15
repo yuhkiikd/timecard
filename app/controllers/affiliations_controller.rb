28f72d88879b9ad4b0f26ae13df592a7300eae9f
yuhkiikd <yuhki.ikd@gmail.com>
2020-02-15 15:46:25 +0000

    006_所属登録機能作成

--- app/controllers/affiliations_controller.rb
+++ app/controllers/affiliations_controller.rb
@@ -1,4 +1,29 @@
 class AffiliationsController < ApplicationController
   def index
+    @affiliations = Affiliation.all
   end
+
+  def new
+    @affiliation = Affiliation.new
+  end
+
+  def create
+    Affiliation.create(affiliation_params)
+    redirect_to affiliations_path
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
+
+private
+
+def affiliation_params
+  params.require(:affiliation).permit(:name)
 end

2216617b9c163dea5a12bd23f065bcedf32a5a24
yuhkiikd <yuhki.ikd@gmail.com>
2020-02-16 11:55:21 +0000

    009_ユーザー詳細追加・フォーム微調整・AffiliationHelper作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
+  belongs_to :affiliation, foreign_key: "affiliation_id"
   has_secure_password
-
+  before_validation { mail.downcase! }
   validates :name, presence: true, length: { maximum: 10 }
   validates :mail, presence: true, length: { maximum: 50 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
+  validates :affiliation_id, presence: true
   validates :password, presence: true, length: { minimum: 8 }
-  before_validation { mail.downcase! }
-  belongs_to :affiliation, foreign_key: "affiliation_id"
 end

0362cd111f55936c23a24af63046ccbc72e9ecdd
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-04 23:06:44 +0000

    028_管理者自身を削除できないよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   belongs_to :affiliation, foreign_key: "affiliation_id"
+  belongs_to :time_card, foreign_key: "time_card_id", dependent: :destroy
   validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, length: { maximum: 50 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
   validates :affiliation_id, presence: true
+  before_destroy :least_one
+
+  private
+
+  def least_one
+    if User.where(admin: :true).count == 1 && admin == true
+      errors[:base] << '管理者権限は最低1つ必要です'
+      throw :abort
+    end
+  end
 end

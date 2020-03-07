59b5af480bf69de47cfb5fecf97054f0385887f1
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-07 16:10:26 +0000

    035_routesのletter_opener記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'affiliations#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
   }

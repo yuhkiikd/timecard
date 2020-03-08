735e288136d87ddddbc5e6a0b7e96cadf523cfa5
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-08 11:08:18 +0000

    040_init unicorn

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,3 @@ end
     name: '営業部',
     )
 end
-
-1.times do |i|
-  User.create!(
-    name: 'admin',
-    email: 'testdikd@gmail.com',
-    password: 'hogehoge',
-    admin: true,
-    affiliation_id: 1,
-    )
-end
-
-1.times do |i|
-  User.create!(
-    name: 'normal',
-    email: 'normal@a.com',
-    password: 'hogehoge',
-    admin: true,
-    affiliation_id: 2,
-    )
-end

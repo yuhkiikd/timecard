50bacb3281e28b83fe2e719af4f0810803264219
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-08 07:05:08 +0000

    036_seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,27 @@
 end
 
 1.times do |i|
+  Affiliation.create!(
+    name: '営業部',
+    )
+end
+
+1.times do |i|
   User.create!(
-    name: 'admin2',
-    email: 'admin2@a.com',
+    name: 'admin',
+    email: 'admin@a.com',
     password: 'hogehoge',
     admin: true,
     affiliation_id: 1,
+    )
+end
+
+1.times do |i|
+  User.create!(
+    name: 'normal',
+    email: 'normal@a.com',
+    password: 'hogehoge',
+    admin: true,
+    affiliation_id: 2,
     )
 end

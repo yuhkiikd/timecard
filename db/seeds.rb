afb817071e4523e43fadf1e1d82bce75e20d5c7b
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-09 09:03:21 +0000

    064_gemfile修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,23 @@ end
     name: '営業部',
     )
 end
+
+1.times do |i|
+  User.create!(
+    name: 'admin',
+    email: 'testdikd@gmail.com',
+    password: 'hogehoge',
+    admin: true,
+    affiliation_id: 1,
+    )
+end
+
+1.times do |i|
+  User.create!(
+    name: 'normal',
+    email: 'boloboloda@gmail.com',
+    password: 'hogehoge',
+    admin: true,
+    affiliation_id: 2,
+    )
+end 

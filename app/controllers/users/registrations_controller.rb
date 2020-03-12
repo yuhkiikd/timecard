08ecd95eb43daa34315ba473702b3b8e893cf9b1
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-12 13:47:19 +0000

    106_revert

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,6 +7,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :logged_in?, only: [:new, :edit]
   before_action :ensure_admin, only: [:new, :edit]
   before_action :creatable?, only: [:new, :create]
+  before_action :editable?, only: [:edit, :update]
 
   # GET /resource/sign_up
   def new
@@ -20,13 +21,58 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/edit
   def edit
-    super
+    render :edit
+    if by_admin_user?(params)
+      self.resource = resource_class.to_adapter.get!(params[:id])
+    else
+      authenticate_scope!
+      super
+    end
   end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def edit
+    if by_admin_user?(params)
+      self.resource = resource_class.to_adapter.get!(params[:id])
+    else
+      authenticate_scope!
+      super
+    end
+  end
+
+
+ def update
+    if by_admin_user?(params)
+      self.resource = resource_class.to_adapter.get!(params[:id])
+    else
+      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    end
+
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    if by_admin_user?(params)
+      resource_updated = update_resource_without_password(resource, account_update_params)
+    else
+      resource_updated = update_resource(resource, account_update_params)
+    end
+
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      if !by_admin_user?(params)
+        bypass_sign_in resource, scope: resource_name
+      end
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # DELETE /resource
   # def destroy
@@ -64,19 +110,68 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
 
-  def sign_up(resource_name, resource)
-    if !current_user_is_admin?
-      sign_in(resource_name, resource)
-    end
+def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def by_admin_user?(params)
+    params[:id].present? && current_user_is_admin?
   end
 
   def current_user_is_admin?
     user_signed_in? && current_user.admin?
   end
 
+  # The path used after sign up.
+  def after_sign_up_path_for(resource)
+    if current_user_is_admin?
+      users_path
+    else
+      super(resource)
+    end
+  end
+
+  # The path used after update.
+  def after_update_path_for(resource)
+    if current_user_is_admin?
+      users_path
+    else
+      super(resource)
+    end
+  end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  def sign_up(resource_name, resource)
+    if !current_user_is_admin?
+      sign_in(resource_name, resource)
+    end
+  end
+  
+  def update_resource_without_password(resource, params)
+    resource.update_without_password(params)
+  end
+
   def creatable?
     raise CanCan::AccessDenied unless user_signed_in?
+
     if !current_user_is_admin?
+      raise CanCan::AccessDenied
+    end
+  end
+
+  def editable?
+    raise CanCan::AccessDenied unless user_signed_in?
+
+    if params[:id].present? && !current_user_is_admin?
       raise CanCan::AccessDenied
     end
   end

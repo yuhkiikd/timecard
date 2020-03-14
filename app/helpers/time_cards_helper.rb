f7b8db41f2ef5a7f04ebf3c6afa7b44f154f8079
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-14 19:03:07 +0000

    129_ec2用に時間表示の調整

--- app/helpers/time_cards_helper.rb
+++ app/helpers/time_cards_helper.rb
@@ -79,13 +79,13 @@ module TimeCardsHelper
 
   def set_user_chart_data
     @chart_days = TimeCard.where(year: @year, month: @month, user_id: @user.id).asc.pluck(:worked_in_at).map{ |item| item.strftime('%Y/%m/%d')}
-    @chart_times = TimeCard.where(year: @year, month: @month, user_id: @user.id).asc.pluck(:overtime).map{ |item| Time.at(item).strftime('%X:%M').to_i}
+    @chart_times = TimeCard.where(year: @year, month: @month, user_id: @user.id).asc.pluck(:overtime).map{ |item| Time.at(item).strftime('%X:%M').to_i - 32400}
     @worked_time = TimeCard.where(year: @year, month: @month, user_id: @user.id).sum(:worked_time) - TimeCard.where(year: @year, month: @month, user_id: @user.id).sum(:breaked_time)
     @overtime = TimeCard.where(year: @year, month: @month, user_id: @user.id).sum(:overtime)
   end
 
   def set_affiliation_chart_data
     @days = TimeCard.where(affiliation_id: @affiliation.id).group_date_asc_day.minimum(:worked_in_at).values.map{ |item| item.strftime('%Y/%m/%d')}
-    @times = TimeCard.where(affiliation_id: @affiliation.id).group_date_asc_day.sum(:overtime).values.map{ |item| Time.at(item).strftime('%X:%M').to_i}
+    @times = TimeCard.where(affiliation_id: @affiliation.id).group_date_asc_day.sum(:overtime).values.map{ |item| Time.at(item).strftime('%X:%M').to_i - 32400}
   end
 end

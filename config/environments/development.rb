3fb6a410b7c60344e2d1b3030731ca83d0b45d9b
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-16 11:05:24 +0000

    140_mail設定中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-
+  require 'base64'
   config.cache_classes = false
 
   config.eager_load = false
@@ -38,5 +38,13 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :login
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,14 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://timecard-ikd.herokuapp.com/' }
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.default_url_options = { host: 'http://13.114.47.119/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :login
+  }
 end

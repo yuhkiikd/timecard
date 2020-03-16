31a86039c5d1db4914420f08f992786ced7a17d6
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-16 13:09:19 +0000

    147_heroku用メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,5 +35,4 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'https://timecard-ikd.herokuapp.com/' }
-  config.action_mailer.delivery_method = :ses
 end

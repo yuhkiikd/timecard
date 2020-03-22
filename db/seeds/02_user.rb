dbaafeb4904d0875050436da868f7de031e75d44
yuhkiikd <yuhki.ikd@gmail.com>
2020-03-22 10:36:09 +0000

    183_seedデータ修正

--- db/seeds/02_user.rb
+++ db/seeds/02_user.rb
@@ -3,19 +3,23 @@
 1.times do |i|
   User.create!(
     name: "admin",
-    email: "testdikd@gmail.com",
+    email: "admin@example.com",
     password: "hogehoge",
     admin: true,
     affiliation_id: 1,
+    confirmation_sent_at: DateTime.new(2020,3,1,0),
+    confirmed_at: DateTime.new(2020,3,1,0)
     )
 end
 
 20.times do |i|
   User.create!(
     name: "normal",
-    email: "normal-#{i + 1}@a.com",
+    email: "normal-#{i + 1}@example.com",
     password: "hogehoge",
     admin: false,
     affiliation_id: i + 1,
+    confirmation_sent_at: DateTime.new(2020,3,1,0),
+    confirmed_at: DateTime.new(2020,3,1,0)
     )
 end
